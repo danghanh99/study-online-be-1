661b1bffff5340a8bc3d1feea1af618061acf253
danghanh99 <ldhanh1999@gmail.com>
2022-02-27 10:47:37 +0700

    typo

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :password, :gender, :job, :created_at, :updated_at
   def job
-    Job.find_by(id: object.job_id) id object.job_id
+    Job.find_by(id: object.job_id) if object.job_id
   end
 end
