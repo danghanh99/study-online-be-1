6f0ecc4deab4830dc8b173fb680b2922305a9539
danghanh99 <ldhanh1999@gmail.com>
2022-02-27 10:50:38 +0700

    category

--- app/serializers/group_full_serializer.rb
+++ app/serializers/group_full_serializer.rb
@@ -1,5 +1,9 @@
 class GroupFullSerializer < ActiveModel::Serializer
-  attributes :id, :started, :name, :code, :url, :description, :members_number, :admin_id, :users, :category_id
+  attributes :id, :started, :name, :code, :url, :description, :members_number, :admin_id, :users, :category
+
+  def category
+    Category.find_by(id: object.category_id) if object.category_id
+  end
 
   def users
     object.users.map do |user|
--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,3 +1,7 @@
 class GroupSerializer < ActiveModel::Serializer
-  attributes :id, :started, :name, :code, :url, :description, :members_number, :admin_id, :category_id
+  attributes :id, :started, :name, :code, :url, :description, :members_number, :admin_id, :category
+
+  def category
+    Category.find_by(id: object.category_id) if object.category_id
+  end
 end
